8373674459dda0f4cce670a785c9270f775fae56
NguyenDucHuy <duchuytv2102@gmail.com>
2024-03-06 02:39:43 +0700

    Light-Dark mode: done, system-mode: not working

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,55 @@
 import Button from '@mui/material/Button';
 import { AccessAlarm, ThreeDRotation } from '@mui/icons-material';
-
 import { useColorScheme } from '@mui/material/styles';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import LightModeIcon from '@mui/icons-material/LightMode';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness';
+
+function ModeSelect() {
+    const { mode, setMode } = useColorScheme();
+    const handleChangeMode = (e) => {
+        const selectedValue = e.target.value;
+        console.log(selectedValue);
+        setMode(selectedValue);
+    };
+
+    return (
+        <FormControl sx={{ m: 1, minWidth: 120 }} size='small'>
+            <InputLabel id='label-dark-light-mode'>Mode</InputLabel>
+            <Select
+                defaultValue=''
+                labelId='label-dark-light-mode'
+                id='dark-light-mode'
+                value={mode}
+                label='Mode'
+                onChange={handleChangeMode}
+            >
+                <MenuItem value='light'>
+                    <div className='flex items-center gap-2'>
+                        <LightModeIcon fontSize='small' />
+                        Light
+                    </div>
+                </MenuItem>
+                <MenuItem value='dark'>
+                    <div className='flex items-center gap-2'>
+                        <DarkModeIcon fontSize='small' />
+                        Dark
+                    </div>
+                </MenuItem>
+                <MenuItem value='system'>
+                    <div className='flex items-center gap-2'>
+                        <SettingsBrightnessIcon fontSize='small' />
+                        System
+                    </div>
+                </MenuItem>
+            </Select>
+        </FormControl>
+    );
+}
 
 function ModeToggle() {
     const { mode, setMode } = useColorScheme();
@@ -19,6 +67,8 @@ function ModeToggle() {
 function App() {
     return (
         <>
+            <ModeSelect />
+            <hr />
             <ModeToggle />
             <hr />
             <p>Hello Duc Huy</p>
