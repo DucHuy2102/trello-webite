36c07e0b329769ca704704d84844c42310e7a2c8
NguyenDucHuy <duchuytv2102@gmail.com>
2024-03-05 17:11:18 +0700

    Config Theme using CssVarsProvider, edit dark-light mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import Button from '@mui/material/Button';
 import { AccessAlarm, ThreeDRotation } from '@mui/icons-material';
 
+import { useColorScheme } from '@mui/material/styles';
+
+function ModeToggle() {
+    const { mode, setMode } = useColorScheme();
+    return (
+        <Button
+            onClick={() => {
+                setMode(mode === 'light' ? 'dark' : 'light');
+            }}
+        >
+            {mode === 'light' ? 'Turn dark' : 'Turn light'}
+        </Button>
+    );
+}
+
 function App() {
     return (
         <>
+            <ModeToggle />
+            <hr />
             <p>Hello Duc Huy</p>
             <Button variant='text'>Text</Button>
             <Button variant='contained'>Contained</Button>
